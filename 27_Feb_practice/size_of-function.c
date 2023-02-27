2b7fffb28a50d6e1825fc87875337237f4e37dae
Bettrix <bettbrian022@gmail.com>
2023-02-27 10:35:34 +0000

    my coding practice on using sizeof operator

---
+++ 27_Feb_practice/size_of-function.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/*
+ * main - Entry point
+ * return(0)
+ *
+ */
+int main(void)
+{
+	printf("Size of char: %lu bytes\n", sizeof(char));
+	printf("Size of int: %ld bytes\n", sizeof(int));
+	printf("Size of float: %ld byes\n", sizeof(float));
+return 0;
+}
+/*
+ * function for printing sizes of int, char and float
+ *
+ */
